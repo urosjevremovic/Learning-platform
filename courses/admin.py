de9078b906b94db6971827d0a82632b9cc654a60
urosjevremovic <jevremovic.uros91@gmail.com>
2018-08-11 12:13:10 +0200

    added fixtures for Subject model

--- courses/admin.py
+++ courses/admin.py
@@ -15,7 +15,7 @@ class ModuleInline(admin.StackedInline):
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['title', 'subject', 'created']
-    list_filter = ['created', 'subject']
+    list_filter = ['created', 'subject', 'owner']
     search_fields = ['title', 'overview']
     prepopulated_fields = {'slug': ('title', )}
     inlines = [ModuleInline]
