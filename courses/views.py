6cc06ce401a1addc1c7a59a4eb4c454eefe59511
urosjevremovic <jevremovic.uros91@gmail.com>
2018-08-12 20:41:38 +0200

    added custom mixins and scatched views for updating, creating and deleting courses

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,46 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from courses.models import Course
 
 
-class ManageCourseListView(ListView):
-    model = Course
-    template_name = 'courses/manage/course/list.html'
+class OwnerMixin:
 
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid()
+
+
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+    template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    success_url = reverse_lazy('manage_course_list')
+
