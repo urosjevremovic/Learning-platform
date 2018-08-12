487e3ee47ee026497828d9a7bf4d89670b5dd18e
urosjevremovic <jevremovic.uros91@gmail.com>
2018-08-12 20:26:39 +0200

    added ListView for Course model

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from courses.models import Course
+
+
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(owner=self.request.user)
