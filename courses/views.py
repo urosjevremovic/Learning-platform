5456a28b06f7ad9716cea9adff8a3da43ed598c1
urosjevremovic <jevremovic.uros91@gmail.com>
2018-08-17 13:23:36 +0200

    added a view for ordering content of each module

--- courses/views.py
+++ courses/views.py
@@ -121,3 +121,11 @@ class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Module.objects.filter(id=id, course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
