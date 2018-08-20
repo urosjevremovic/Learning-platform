728f2b5a573291db2c5e79ffc4900f213e54342f
urosjevremovic <jevremovic.uros91@gmail.com>
2018-08-20 17:01:13 +0200

    added view for listing all available courses for given subject, or all courses if no subject is given

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
 from django.apps import apps
+from django.db.models import Count
 from django.forms import modelform_factory
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
@@ -8,7 +9,7 @@ from django.views.generic.base import View, TemplateResponseMixin
 
 from courses.forms import ModuleFormSet
 from courses.mixins import OwnerCourseMixin, OwnerCourseEditMixin
-from courses.models import Course, Module, Content
+from courses.models import Course, Module, Content, Subject
 
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
@@ -129,3 +130,16 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects, 'subject': subject, 'courses': courses})
