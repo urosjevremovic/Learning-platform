b5cfd5fcdc70f7ffcc91d3b62c4de8b613e7a803
urosjevremovic <jevremovic.uros91@gmail.com>
2018-08-21 16:55:51 +0200

    added template for detail view of each course

--- courses/urls.py
+++ courses/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('module/<int:module_id>/', ModuleContentListView.as_view(), name='module_content_list'),
     path('module/order/', ModuleOrderView.as_view(), name='module_order'),
     path('content/order/', ContentOrderView.as_view(), name='content_order'),
-    path('subject/<slug:subject>)/', CourseListView.as_view(), name='course_list_subject'),
-
+    path('subject/<slug:subject>/', CourseListView.as_view(), name='course_list_subject'),
+    path('subject/', CourseListView.as_view(), name='course_list_subject'),
     path('<slug:slug>/', CourseDetailView.as_view(), name='course_detail'),
 ]
