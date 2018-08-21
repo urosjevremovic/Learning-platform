59a050579aae637fb3ff29c7d6b507120f83f1d6
urosjevremovic <jevremovic.uros91@gmail.com>
2018-08-21 18:28:40 +0200

    changed form for user registration by adding method for checking if passwords matched and properly displaying message to user if password did not match, also added registration template and changed redirection of UserRegistrationView for now

--- user/forms.py
+++ user/forms.py
@@ -10,23 +10,24 @@ class LoginForm(forms.Form):
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
-    password_2 = forms.CharField(label="Repeat your password", widget=forms.PasswordInput)
+    password_2 = forms.CharField(label="Repeat your password", widget=forms.PasswordInput, required=False)
     email = forms.EmailField(label='Email address', required=True)
     first_name = forms.CharField(max_length=120, required=True)
     last_name = forms.CharField(max_length=120, required=True)
-    data_of_birth = forms.DateTimeField()
-    location = forms.CharField(max_length=120)
     photo = forms.ImageField()
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'date_of_birth', 'location', 'photo')
 
-    def clean(self):
-        cd = super().clean()
-        email = cd.get('email')
-        qs = User.objects.filter(email__iexact=email)
-        if cd.get('password') != cd.get('password_2'):
-            raise forms.ValidationError("Passwords didn't match. Please try again")
-        if qs.exists():
-            raise forms.ValidationError("Sorry, this email is already in use.")
+    def clean_password_2(self):
+        password = self.cleaned_data.get('password')
+        password_2 = self.cleaned_data.get('password_2')
+
+        if not password_2:
+            msg = "You must confirm your password"
+            self.add_error('password', msg)
+        if password != password_2:
+            msg = "Your passwords did not match"
+            self.add_error('password', msg)
+        return password_2
