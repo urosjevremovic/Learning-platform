2da3014b1e906953a7abfb462a0fffee45f7bc6d
urosjevremovic <jevremovic.uros91@gmail.com>
2018-08-23 18:24:46 +0200

    added view for viewing details of each Course that User is enrolled in

--- courses/views.py
+++ courses/views.py
@@ -157,10 +157,29 @@ class CourseDetailView(DetailView):
         return context
 
 
-class StudentCourseListVIew(LoginRequiredMixin, ListView):
+class StudentCourseListView(LoginRequiredMixin, ListView):
     model = Course
     template_name = 'users/course/list.html'
 
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'users/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+
+        else:
+            context['module'] = course.modules.all()[0]
+        return context
